f871e8a6935159a036a258c73abb5c3993a4682d
Mimi1008-tech <mimi1008.tech@gmail.com>
2023-07-24 16:10:25 +0900

    change header bar link

--- template/header.template.php
+++ template/header.template.php
@@ -7,7 +7,7 @@
             <ul>
                 <li><a href="/~s2311108/comlit14/">トップ</a></li>
                 <li><a href="/~s2311108/comlit14/edit">記事を書く</a></li>
-                <li><a href="/~s2311108/comlit14/article/?id=1">このページについて</a></li>
+                <li><a href="/~s2311108/comlit14/about/">このページについて</a></li>
             </ul>
         </div>
     </nav>
